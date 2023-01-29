7fb2b3ddf497aa04c6fe6bb6b4ed2ea1d1bb066b
mirageruler <khoinguyen1411998@gmail.com>
2023-01-29 15:14:53 +0700

    add_avl_implementation

--- data_structures/tree/share.go
+++ data_structures/tree/share.go
@@ -1,4 +1,4 @@
-package tree
+package main
 
 // Node defines a node in any tree structure
 // Note that we only support integer value for the Node's value so we can implement the tree structures with ease.
@@ -11,4 +11,11 @@ type Node struct {
 // NewNode returns a Node contains val as `Val` and nil pointer for `Left` and `Right`
 func NewNode(val int) *Node {
 	return &Node{Val: val}
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
